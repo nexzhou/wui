1ae974781ef5566890cf7ec9b759574c59241d3a
nexzhou <36834799+nexzhou@users.noreply.github.com>
2019-08-19 08:48:07 +0800

    wui
    article main

--- src/pages/ArticlePage/ArticleMain.js
+++ src/pages/ArticlePage/ArticleMain.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 export default class ArticleMain extends React.Component {
-  
-
-
+  render() {
+    return (
+      <div class="main-content article-content">
+        <h2>Article</h2>
+      </div>
+    );
+  }
 }
--- src/pages/ArticlePage/index.js
+++ src/pages/ArticlePage/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 
-import MyNav from '../../parts/MyNav';
+import MyNav from "../../parts/MyNav";
+import ArticleMain from "./ArticleMain";
 
 const defaultTabs = [
   { path: "/", icon: "rollback" },
@@ -12,8 +13,8 @@ const defaultTabs = [
 export default class ArticlePage extends React.Component {
   render() {
     return (
-      <div className='layout-wrapper'>
-      <MyNav tabs={defaultTabs}/>
+      <div className="layout-wrapper">
+        <MyNav tabs={defaultTabs} />
         <div>article</div>
       </div>
     );
